0df0523fc960c54b9881fd9c58d15d80965f606d
yx77777736 <yusufalikhil7@gmail.com>
2026-06-14 17:42:14 +0400

    average.py

---
+++ average.py
@@ -0,0 +1,9 @@
+tree1=5
+tree2=15
+tree3=21
+tree4=32
+tree5=50
+sum=tree1+tree2+tree3+tree4+tree5
+print("sum of 5 trees",sum)
+ave=sum/5
+print("average of 5 trees",ave)
---
+++ operator.py
@@ -0,0 +1,11 @@
+x=10
+y=20
+print("x : ",x)
+print("y : ",y)
+print("x+y : ",x+y)
+print("x*y : ",x*y)
+print("x-y : ",x-y)
+print("x/y : ",x/y)
+print("x//y : ",x//y)
+print("x%y : ",x%y)
+print("x**y : ",x**y)
