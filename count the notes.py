9cbd5615921a671b237076e78afac2c5a85dac41
yx77777736 <yusufalikhil7@gmail.com>
2026-06-21 17:57:33 +0400

    count1

---
+++ count the notes.py
@@ -0,0 +1,3 @@
+amount=int(input("enter withdrawl amount "))
+note1=amount//100
+print("100 notes",note1)
---
+++ marks percentage.py
@@ -0,0 +1,8 @@
+math=int(input("enter math marks "))
+hindi=int(input("enter hindi marks "))
+science=int(input("enter science marks "))
+english=int(input("enter english marks "))
+sum=math+hindi+science+english
+print("sum of marks",sum)
+perc=(sum / 400)*100
+print("percentage of 4 subject",perc)
